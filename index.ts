554baaa637ffc3f845ca23fab38e4a58afaaac87
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-02-15 10:01:46 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
+/**
+ * The program shows how to get input
+ *   and deal with numbers.
+ *
+ * By:      Curtis Edwards
+ * Version: 1.0
+ * Since:   2024-02-15
+ */
 
+import { createPrompt } from 'bun-promptx'
+
+const lengthString = createPrompt('What length do you want your logs to be?(0.25, 0.5, 1): ')
+let lengthNumber = parseInt(ageString.value)
+let numberOfLogs = 1100 / (20 / lengthNumber)
+console.log('The truck can carry ${numberOfLogs} logs if they are ${lengthNumber}m log')
+
+console.log('\nDone.')
