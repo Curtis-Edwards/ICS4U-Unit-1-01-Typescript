22a78a0578cd928360d6e81bedc8a619655cc38d
Curtis Edwards <curtis-edwards>
2024-02-16 08:34:49 -0500

    Added code

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 /**
- * The program shows how to get input
- *   and deal with numbers.
+ * The program shows how to many logs can fit
+ *   on a truck given the length of the logs.
  *
  * By:      Curtis Edwards
  * Version: 1.0
@@ -12,6 +12,8 @@ import { createPrompt } from 'bun-promptx'
 const lengthString = createPrompt('What length do you want your logs to be?(0.25, 0.5, 1): ')
 let lengthNumber = parseInt(lengthString.value)
 let numberOfLogs = 1100 / (20 / lengthNumber)
-console.log('The truck can carry > ${numberOfLogs} logs if they are > ${lengthNumber}m log')
+console.log('The truck can carry ${numberOfLogs} logs if they are ${lengthNumber}m long')
+
+
 
 console.log('\nDone.')
